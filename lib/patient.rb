c20d7ea81db99e347f8edb3c7e874f4434542073
cd1412 <chiduong1412@gmail.com>
2022-04-12 02:33:07 -0500

    Completed assignment

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,17 +11,17 @@ class Patient
     def self.all
       @@all
     end
-  
+
+    def new_appointment(date, doctor)
+      Appointment.new(date, self, doctor)
+    end
+
     def appointments
       Appointment.all.select {|appointment| appointment.patient == self}
     end
   
     def doctors
       appointments.collect {|appointment| appointment.doctor}
-    end
-  
-    def new_appointment(doctor, date)
-      Appointment.new(self, date, doctor)
     end
   
   end 
